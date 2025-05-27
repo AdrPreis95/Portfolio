9c39eb2fd74e03649c2b6f63212cafd377f81798
AdrPreis95 <adrianpreis86@gmail.com>
2025-05-28 00:56:21 +0200

    added AOS Library Anomation Scrolleffects for all Sections

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,33 @@
+import { Component, OnInit, Inject, PLATFORM_ID } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
-import { Component } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import * as AOS from 'aos';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'portfolio-classic';
-  isLegalNotice = false; 
+  isLegalNotice = false;
 
-  constructor(private router: Router) {
-    this.router.events.subscribe(event => {
-      if (event instanceof NavigationEnd) {
-        this.isLegalNotice = event.url === '/legal-notice';
-      }
-    });
+  constructor(private router: Router, @Inject(PLATFORM_ID) private platformId: Object) {}
+
+  ngOnInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      AOS.init({
+        duration: 800,
+        easing: 'ease-in-out',
+        once: false
+      });
+
+      this.router.events.subscribe(event => {
+        if (event instanceof NavigationEnd) {
+          this.isLegalNotice = event.url === '/legal-notice';
+          setTimeout(() => AOS.refresh(), 300);
+        }
+      });
+    }
   }
 }
